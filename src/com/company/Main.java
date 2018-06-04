edf8b424a08bf62ba3dc8c008628e1752239dd3e
Kolangi <kolangi.kelly@yahoo.com>
2018-06-04 11:30:14 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+
+
+package com.company;
+import java.text.ParseException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) throws ParseException {
+        Scanner input = new Scanner(System.in);
+
+        SimpleDateFormat inputData = new SimpleDateFormat();
+        System.out.println("Whats Todays Date ? ");
+
+        Date date = new Date();
+        String userinput ="";
+        userinput =input.nextLine();
+
+
+        String dateFormat ="dd/MM/yyyy";
+        inputData = new SimpleDateFormat(dateFormat);
+
+        Date userInputDt=inputData.parse(userinput);
+
+        while (userInputDt.after(new Date())){
+            System.out.println(" Enter Date Again ");
+            userinput =input.nextLine();
+
+            inputData = new SimpleDateFormat(dateFormat);
+            userInputDt=inputData.parse(userinput);
+
+        } System.out.println("OK");
+
+
+    }
+}
+
+
+
+
